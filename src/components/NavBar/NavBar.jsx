425828e3550dda6d416fef58bf9d6c2701120de6
Ivo Nicolás Chapochnikoff <chapochnikoffivo@gmail.com>
2023-03-27 14:55:40 +0200

    mediaQueryNavbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,6 +4,8 @@ import { storage } from '../../firebase/firebase'
 import {NavLink, useLocation} from "react-router-dom";
 import './NavBar.css';
 import NavBarList from '../NavBarList/NavBarLis';
+import { Menu} from 'react-feather';
+
 
 
 export default function NavBar() {
@@ -20,12 +22,18 @@ useEffect(() => {
 
 return (
     <nav className={`navbar ${path === "/" ? "home-page" : "other-page"}`}>
+        <input type='checkbox' id='check' />
+            <label htmlFor='check' className='checkBtn'>
+                <Menu/>
+            </label>
         <NavLink to='/' className={`navbar ${path === "/" ? "Logo" : "other-logo"}`}>
                 <img className={`navbar ${path === "/" ? "logoImg" : "other-img"}`} src={logoUrl} alt="logo" />
         </NavLink>
+        <div className='aboutMeDiv'>
+        <NavLink to={`/aboutMe`} className='aboutme' activeclassname='active'>  Sobre mí </NavLink>
+        <span className='aboutme'>Recursos</span>
+        </div>
         <div className='wow redesSociales animate__animated animate__backInDown'>
-            <NavLink to={`/aboutMe`} className='aboutme' activeclassname='active'>  Sobre mí </NavLink>
-            <span className='aboutme'>Recursos</span>
             <NavBarList/>
             </div>
         </nav>
